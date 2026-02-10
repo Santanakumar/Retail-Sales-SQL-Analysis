0bc6d87d188289e742aa23191a048f4caebb32ec
Santanakumar <santanakumarinukollu@gmail.com>
2026-02-11 00:17:33 +0530

    Update create_table.sql

--- sql_queries/create_table.sql
+++ sql_queries/create_table.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE online_retail(
+	InvoiceNo NVARCHAR(50),
+	StockCode NVARCHAR(50),
+	Description NVARCHAR(255),
+	Quantity INT,
+	InvoiceDate DATETIME2(7),
+	UnitPrice DECIMAL(10,2),
+	CustomerID FLOAT NULL,
+	Country NVARCHAR(50)
+);
